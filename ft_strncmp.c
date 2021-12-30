52dafc7cfb8e6f57e7c0017358dec0b2f3b6499b
Benjamin <bperraud@ulb.be>
2021-12-30 02:08:14 +0100

    change for strncmp for NULL param

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -13,13 +13,12 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+	if (!n)
+		return (0);
 	while (n-- && *s1 && *s2 && *s1 == *s2)
 	{
 		s1++;
 		s2++;
 	}
-	if (n)
-		return (*s1 - *s2);
-	else
-		return (0);
+	return (*s1 - *s2);
 }

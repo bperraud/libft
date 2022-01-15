4d590be9ddd5ed807e5e4323303af78ef42f6da3
bperraud <74benji@live.fr>
2022-01-15 20:34:55 +0100

    lstclear

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -5,10 +5,11 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: bperraud <bperraud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/17 02:42:51 by bperraud          #+#    #+#             */
-/*   Updated: 2021/11/17 11:32:15 by bperraud         ###   ########.fr       */
+/*   Created: 2022/01/15 20:34:35 by bperraud          #+#    #+#             */
+/*   Updated: 2022/01/15 20:34:38 by bperraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
 void	ft_rec_lstclear(t_list **lst, void (*del)(void*))
@@ -18,14 +19,11 @@ void	ft_rec_lstclear(t_list **lst, void (*del)(void*))
 	list = *lst;
 	if (!list->next)
 	{
-		(del)(*lst);
-		free(*lst);
+		ft_lstdelone(*lst, del);
 		*lst = NULL;
 	}
 	else
-	{
 		ft_rec_lstclear(&list->next, (del));
-	}
 }
 
 void	ft_lstclear(t_list **lst, void (*del)(void*))

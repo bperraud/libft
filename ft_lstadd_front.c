415f54617f43560847aca45c4941d887db0e99f1
bperraud <74benji@live.fr>
2022-01-15 20:35:30 +0100

    lstaddfront

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -5,13 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: bperraud <bperraud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/17 02:42:51 by bperraud          #+#    #+#             */
-/*   Updated: 2021/11/17 11:32:15 by bperraud         ###   ########.fr       */
+/*   Created: 2022/01/15 20:35:07 by bperraud          #+#    #+#             */
+/*   Updated: 2022/01/15 20:35:11 by bperraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
-void ft_lstadd_front(t_list **alst, t_list *new)
+void	ft_lstadd_front(t_list **alst, t_list *new)
 {
 	t_list	*list;
 
